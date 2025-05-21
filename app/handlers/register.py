6441e51586ca9a064400764a7ff7c4ab9da1720d
Erik Kling <kling.erik@icloud.com>
2025-05-21 23:13:03 +0200

    Easier code without database

--- app/handlers/register.py
+++
@@ -1,9 +0,0 @@
-def handle_registration(user_number: str, message_text: str):
-    """
-    Collects and stores initial registration info from the user.
-    """
-    from db.users import insert_user
-    from app.services.twilio import send_whatsapp_message
-
-    if insert_user(user_number, message_text):
-        send_whatsapp_message(user_number, "Welcome to WhatsUp! Please tell us your favorite topic.")
